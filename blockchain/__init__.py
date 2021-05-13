e44a4e34b7171eda7e0d80edd9df5ccc433e6a23
Herson Oliveira <oliveira.herson@gmail.com>
2021-05-13 22:19:09 +0200

    Update view to get chain

--- blockchain/__init__.py
+++ blockchain/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
-from blockchain.views import mine_blueprint
+from blockchain.views import mine_bp, transactions_bp, chain_bp
+from blockchain.models import Blockchain
 
 
 def create_app(config):
     app = Flask(__name__)
 
     # Register blueprints
-    app.register_blueprint(mine_blueprint)
+    app.register_blueprint(mine_bp)
+    app.register_blueprint(transactions_bp)
+    app.register_blueprint(chain_bp)
 
+    # Create blockchain instance
+    app.blockchain = Blockchain()
+    
     return app
--- blockchain/views.py
+++ blockchain/views.py
@@ -1,8 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, current_app
 
-mine_blueprint = Blueprint("mine", __name__, url_prefix="/mine")
+mine_bp = Blueprint("mine", __name__, url_prefix="/mine")
+transactions_bp = Blueprint("transactions", __name__, url_prefix="/transactions")
+chain_bp = Blueprint("chain", __name__, url_prefix="/chain")
 
 
-@mine_blueprint.route("/")
+@mine_bp.route("/", methods=["GET"])
 def mine():
     return "Mine a new block"
+
+
+@transactions_bp.route("/new", methods=["POST"])
+def new_transaction():
+    return "New transaction"
+
+
+@chain_bp.route("/", methods=["GET"])
+def full_chain():
+    response = {
+        "chain": current_app.blockchain.chain,
+        "length": len(current_app.blockchain.chain),
+    }
+    return response, 200
